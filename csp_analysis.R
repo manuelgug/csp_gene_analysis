51ba6769c29104a8a248d44c12a617f175791b3e
Manuel García-Ulloa <36465310+manuelgug@users.noreply.github.com>
2024-03-27 09:29:07 +0100

    Add files via upload

---
+++ csp_analysis.R
@@ -0,0 +1,346 @@
+
+library(ggplot2)
+library(dplyr)
+
+
+######################################################################
+#-----------------------INPUT AND PREP DATA--------------------------#
+######################################################################
+
+#######################################################
+# 1.- metadata
+#######################################################
+
+db <- readxl::read_excel('HFS22_ID_run_list_csp_120324.xlsx')
+colnames(db)[1]<- c("NIDA2")  #just to avoid changing variables in the script from Simone's analysis
+
+length(unique(db$NIDA2))
+
+# 1a.- complete run_id column with data from the actual runs
+
+# Initialize an empty data frame to store the results
+result_df <- data.frame(NIDA2 = character(), NEW_run_id = character(), stringsAsFactors = FALSE)
+
+directory_path <- "../results_v0.1.8_RESMARKERS_FIX/"
+
+# Create a progress bar
+pb <- progress_bar$new(
+  format = "[:bar] :percent ETA: :eta",
+  total = length(dir_ls(path = directory_path, regexp = "_RESULTS_v0.1.8$"))
+)
+
+# Iterate through the folders ending with _RESULTS_v0.1.8
+for (folder_path in dir_ls(path = directory_path, regexp = "_RESULTS_v0.1.8$")) {
+  pb$tick()  # Update progress bar
+  
+  folder_name <- path_file(folder_path)
+  file_path <- file.path(folder_path, "amplicon_coverage.txt")
+  
+  cat("\n")
+  print(folder_name)
+  
+  # Read the contents of /quality_report/amplicon_stats.txt
+  if (file.exists(file_path)) {
+    sample_coverage_content <- readLines(file_path)
+    
+    # Truncate each line after the first tab (\t) and return unique values and edit nida format to match the one from the db
+    truncated_values <- unique(sapply(strsplit(sample_coverage_content, "\t"), function(x) x[1]))
+    truncated_values <- gsub("_S.*$", "", truncated_values)
+    truncated_values <- gsub("_", ".", truncated_values)
+    truncated_values <- gsub("-", ".", truncated_values)
+    truncated_values <- gsub("N", "", truncated_values)
+    
+    # Extract NIDA2 from runs using grep
+    nida_values <- grep(paste(db$NIDA2, collapse = "|"), truncated_values, value = TRUE) #not all samples from runs are needed, only those in the db, hence this
+    
+    # Create a data frame with NIDA2 and folder_name
+    if (length(nida_values) > 0) {
+      temp_df <- data.frame(NIDA2 = nida_values, NEW_run_id = folder_name, stringsAsFactors = FALSE)
+      
+      # Append the results to the main data frame
+      result_df <- rbind(result_df, temp_df)
+    }
+  }
+}
+
+result_df$NEW_run_id <- sub("_RESULTS_v0.1.8$", "", result_df$NEW_run_id)
+
+#this should be TRUE if the same number of nidas is in the db and the results from the grep
+length(result_df$NIDA2) == length(db$NIDA2) #it's not....
+length(result_df$NIDA2) - length(db$NIDA2)
+
+#check for repeated nidas 
+repeated_nidas <- names(table(result_df$NIDA2)[table(result_df$NIDA2) > 1]) #there are duplicate nidas OOF
+repeated_nidas_df<- result_df[result_df$NIDA2 %in% repeated_nidas,]
+repeated_nidas_df <- repeated_nidas_df[order(repeated_nidas_df$NIDA2), ]
+length(repeated_nidas_df$NIDA2)
+length(unique(repeated_nidas_df$NIDA2))
+
+#ask team about these nidas
+write.csv(repeated_nidas_df, "repeated_nidas.csv", row.names = F)
+
+
+#######################################################
+# 2.- genomic data from all runs (allele data, resmarkers, haplos?)
+#######################################################
+runs <- unique(paste0(result_df$NEW_run_id, "_RESULTS_v0.1.8_FILTERED"))
+
+folder_path <- paste0("../results_v0.1.8_RESMARKERS_FIX/", runs)
+allele_data_files <- file.path(folder_path, "allele_data_global_max_0_filtered.csv")
+
+# Import filtered allele data tables to the list
+allele_data_list <- list()
+for (file in allele_data_files) {
+  allele_data <- read.csv(file)
+  allele_data_list <- append(allele_data_list, list(allele_data))
+}
+
+#format the imported dfs
+for (i in seq_along(allele_data_list)) {
+  df <- allele_data_list[[i]]
+  
+  df$sampleID <- gsub("_S.*$", "", df$sampleID)
+  df$sampleID <- gsub("_", ".", df$sampleID)
+  df$sampleID <- gsub("-", ".", df$sampleID)
+  df$sampleID <- gsub("N", "", df$sampleID)
+  df$sampleID <- gsub("\\.0", "", df$sampleID)
+  
+  colnames(df)[1] <- "sample_id"
+  
+  df <- df %>% ### TEHERE IS A BUG WITH THE MASK THAT GENERATES MORE ALLELES THAN THERE REALLY ARE. THIS SNIPPET OF CODE COLLAPSES REPETITIONSAND SUMS THE READS AND FREQS. CRITICAL!!!
+    group_by(sample_id, locus, pseudo_cigar) %>%
+    summarize(reads = sum(reads),
+              norm.reads.locus = sum(norm.reads.locus),
+              Category = first(Category)) %>%
+    mutate(allele = paste(locus, ".", row_number(), sep = ""))
+  
+  
+  # Update the modified data frame back in the list
+  df<- as.data.frame(df)
+  allele_data_list[[i]] <- df
+}
+
+names(allele_data_list) <- runs
+
+#get rid of replicate nidas keep the sample with the most reads across runs for each one of the replicates
+sum_reads <- function(df) {
+  aggregate(reads ~ sample_id, data = df, sum)
+}
+
+summed_reads <- lapply(allele_data_list, sum_reads)
+
+# Change colnames of reads for the name of the respective df
+summed_reads <- lapply(names(summed_reads), function(df_name) {
+  df <- summed_reads[[df_name]]
+  names(df)[2] <- df_name
+  return(df)
+})
+
+# Merge all data frames by sample_id
+merged_df_dups <- Reduce(function(x, y) merge(x, y, by = "sample_id", all = TRUE), summed_reads)
+
+#keep rows with replicates
+merged_df_dups <- merged_df_dups[rowSums(is.na(merged_df_dups)) < length(colnames(merged_df_dups)) - 2, ] #keep rows with more than 14 NAs (that is, that have reads in more than one run)
+
+# Exclude the first column and find the column with the maximum value for each row
+merged_df_dups$BEST_RUN <- colnames(merged_df_dups)[apply(merged_df_dups[-1], 1, which.max) + 1]
+
+#remove replicates, keep the best
+for (i in 1:nrow(merged_df_dups)) {
+  best_run <- merged_df_dups$BEST_RUN[i]
+  sample_id <- merged_df_dups$sample_id[i]
+  
+  # Loop through allele_data_list
+  for (j in seq_along(allele_data_list)) {
+    df <- allele_data_list[[j]]
+    
+    # Exclude the df named after BEST_RUN
+    if (names(allele_data_list)[j] != best_run) {
+      allele_data_list[[j]] <- df[!(df$sample_id %in% sample_id), ]
+    }
+  }
+}
+
+#final check:
+summed_reads <- lapply(allele_data_list, sum_reads)
+summed_reads <- lapply(names(summed_reads), function(df_name) {
+  df <- summed_reads[[df_name]]
+  names(df)[2] <- df_name
+  return(df)
+})
+merged_df_dups <- Reduce(function(x, y) merge(x, y, by = "sample_id", all = TRUE), summed_reads)
+merged_df_dups <- merged_df_dups[rowSums(is.na(merged_df_dups)) < length(colnames(merged_df_dups)) - 2, ] #keep rows with more than 14 NAs (that is, that have reads in more than one run)
+
+if (dim(merged_df_dups)[1] == 0){
+  print("NO MORE REPLICATES.")
+}else{
+  "grab a coffee"
+}
+
+# Define a function to filter rows based on criteria: remove controls, basically
+filter_rows <- function(df) {
+  # Filter rows based on criteria
+  filtered_df <- df[!grepl("^[A-Za-z]|3D", df$sample_id), ]
+  return(filtered_df)
+}
+
+# Apply the filter_rows function to each dataframe in allele_data_list
+allele_data_list <- lapply(allele_data_list, filter_rows)
+
+#visual check:
+for (df in allele_data_list) {
+  cat("sample size:", as.character(length(unique(df$sample_id))))
+  cat("\n")
+  print(unique(df$sample_id))
+  
+}
+
+#save allele_data_list
+saveRDS(allele_data_list, "allele_data_list.RDS")
+
+#######################################################
+# 3.- GENOMIC + DB MERGING, FILTERING ETC. (DATA PREP)
+#######################################################
+
+allele_data_list <- readRDS("allele_data_list.RDS")
+
+# concat all dataframes together
+combined_df <- bind_rows(allele_data_list)
+
+# calculate n.alleles for each locus of each sample if not done already during contaminant filtering
+if (!("n.alleles" %in% colnames(combined_df))){
+  combined_df <- combined_df %>%
+    group_by(sample_id, locus) %>%
+    mutate(n.alleles = n_distinct(pseudo_cigar))
+}
+
+# merge with metadata
+colnames(combined_df)[1]<- c("NIDA2")
+combined_df_merged <- merge(combined_df, db[c("NIDA2", "year", "province", "region")], by="NIDA2", all.y =T) #forcing adding all db nidas
+
+#check for columns with NAs. THESE SAMPLES WERE BAD QUALITY AND THUS FILTERED OUT DURING CONTAMINANTS FILTERING
+removed_samples <- combined_df_merged[is.na(combined_df_merged$Category),]
+removed_samples$NIDA2
+
+# Remove rows with NIDA2 matching values in removed_samples
+combined_df_merged <- combined_df_merged[!combined_df_merged$NIDA2 %in% removed_samples$NIDA2, ]
+
+
+#sanity check
+if (sum(is.na(combined_df_merged[, !colnames(combined_df_merged) %in% "seasonality"])) == 0) {
+  print("No NAs ✔")
+} else {
+  print("grab a coffee.")
+}
+
+if( sum(!(combined_df_merged$NIDA2 %in% db$NIDA2)) == 0){
+  print("All nidas in combined_merged_df are also the metadata db. No weird samples ✔")
+}else{
+  print("grab another coffee.")
+}
+
+
+## FURTHER FILTERING
+
+# 1) MAF filering (< 0.01)
+combined_df_merged <- combined_df_merged[combined_df_merged$norm.reads.locus  > 0.01, ]
+
+# 2) check for coverage (>100 loci with >threshold reads)
+# Define thresholds for read depth
+thresholds <- c(25, 50, 100, 200)
+count_list <- list()
+
+# Loop over each threshold
+for (threshold in thresholds) {
+  # Calculate unique loci counts for the current threshold
+  count <- combined_df_merged %>%
+    group_by(NIDA2, locus) %>%
+    summarize(total_reads = sum(reads)) %>%
+    group_by(NIDA2) %>%
+    filter(total_reads > threshold) %>%
+    summarize(!!paste("unique_loci_", threshold, sep = "") := n_distinct(locus))
+  
+  count_list[[paste("count_", threshold, sep = "")]] <- count
+}
+
+result_df <- Reduce(function(x, y) left_join(x, y, by = "NIDA2"), count_list)
+
+#count cells above 100 for each column with the "unique" substring: here, i'm calculating the sample size for each reads count threshold using 100 loci as cutoff: samples with <100 read count per loci below the threshold should be removed
+count_above_100 <- function(x) sum(x >= 100, na.rm = TRUE)
+unique_columns <- grep("unique", colnames(result_df), value = TRUE)
+
+count_results <- result_df %>%
+  summarise_at(vars(unique_columns), count_above_100)
+
+count_results
+
+# decided going with a threshold of >= 100 read depth,
+samples_to_keep <- result_df[result_df$unique_loci_100 >= 100, ]$NIDA2
+
+combined_df_merged <- combined_df_merged[combined_df_merged$NIDA2 %in% samples_to_keep, ]
+
+
+# 3) remove bad loci: those that are not in at least 100 samples with a read depth of 100. 
+# Group by NIDA2 and locus, then summarize the total reads
+locus_read_depth <- combined_df_merged %>%
+  group_by(NIDA2, locus) %>%
+  summarize(read_depth = sum(reads))
+
+# Count the number of samples with read depth greater than 100 for each locus
+locus_counts <- locus_read_depth %>%
+  group_by(locus) %>%
+  summarize(samples_above_100 = sum(read_depth > 100))
+
+loci_to_keep <- locus_counts$locus
+
+combined_df_merged <- combined_df_merged[combined_df_merged$locus %in% loci_to_keep, ]
+
+#recount n.alleles after filtering
+combined_df_merged <- combined_df_merged %>%
+  group_by(NIDA2, locus) %>%
+  mutate(n.alleles = n_distinct(pseudo_cigar))
+
+combined_df_merged <- as.data.frame(combined_df_merged)
+
+
+## Keep only csp loci: c("Pf3D7_03_v3-221185-221419-1B", "Pf3D7_03_v3-221463-221712-1B", "Pf3D7_03_v3-221295-221532-2")
+
+csp_amps <- c("Pf3D7_03_v3-221185-221419-1B", "Pf3D7_03_v3-221463-221712-1B", "Pf3D7_03_v3-221295-221532-2")
+
+combined_df_merged_csp <- combined_df_merged[combined_df_merged$locus %in% csp_amps,]
+
+#rename alleles
+combined_df_merged_csp$allele <- paste0(combined_df_merged_csp$locus, "_", combined_df_merged_csp$pseudo_cigar)
+
+# for cabo delgado
+combined_df_merged_csp$province <- gsub(" ", "_", combined_df_merged_csp$province) 
+
+
+# FILTERING RESULTS
+SS <- length(unique(combined_df_merged_csp$NIDA2))
+cat("Final sample size is", as.character(SS))
+
+LC <- length(unique(combined_df_merged_csp$locus))
+cat("Loci count is", as.character(LC))
+
+#save allele_data_list
+saveRDS(combined_df_merged_csp, "combined_df_merged_csp_only.RDS")
+
+
+#######################################################
+# 4.- CHECK SAMPLE SIZES
+#######################################################
+
+combined_df_merged_csp <- readRDS("combined_df_merged_csp_only.RDS")
+
+sample_size_provinces <- combined_df_merged_csp %>%
+  group_by(province) %>%
+  summarise(unique_NIDA2_count = n_distinct(NIDA2))
+
+sample_size_provinces
+
+sample_size_regions <- combined_df_merged_csp %>%
+  group_by(year, region) %>%
+  summarise(unique_NIDA2_count = n_distinct(NIDA2))
+
+sample_size_regions
